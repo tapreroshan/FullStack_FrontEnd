3c285789a4830502dde39f9b3a1a273d89370af5
Roshan Tapre <roshantapre666@gmail.com>
2025-01-27 22:18:54 +0530

    basic frontend complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JS (includes Popper.js)
+
+import React from "react";
 import './App.css'
+// import Navbar from "./Components/Navbar";
+import Home from "./Components/Pages/Home";
+import { BrowserRouter as Router, Routes,Route  } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+   <Router>  
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   </Router>
+  );
 }
 
-export default App
+
+
+
+export default App;
---
+++ Frontend/src/Components/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Body from './Body'
+
+function Home() {
+  return (
+    <div>
+       <Navbar />
+       <div><Body /></div>
+       <div>footer</div>
+    </div>
+  )
+}
+
+export default Home
